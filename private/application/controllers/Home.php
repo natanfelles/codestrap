408b4a08a14236919ae35fbb4a6cea83babd067d
Natan Felles <natanfelles@gmail.com>
2016-03-27 08:58:51 -0300

    Updated default controller route

---
+++ private/application/controllers/Home.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Codestrap
+ *
+ * @package    Codestrap
+ * @subpackage Controller
+ * @category   Home
+ * @author     Natan Felles
+ * @link       https://github.com/natanfelles/codestrap
+ * @since      Version 1.0.0
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index()
+	{
+		echo 'Hello, world!';
+	}
+
+}
