a66af299467375b1d50cb2c76482ed0861d9c522
Natan Felles <natanfelles@gmail.com>
2016-03-28 18:55:10 -0300

    Add  Upload system

---
+++ private/application/controllers/Upload.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Codestrap
+ *
+ * @package    Codestrap
+ * @author     Natan Felles
+ * @link       https://github.com/natanfelles/codestrap
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Class Upload
+ * Simple single file upload
+ */
+class Upload extends CI_Controller {
+
+	/**
+	 * @var string $allowed_types Allowed file types to upload
+	 */
+	protected $allowed_types = 'gif|jpg|png|flv|mp3|mp4';
+
+
+	/**
+	 * Loads the necessary librarys
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('upload');
+	}
+
+
+	/**
+	 * Loads the page and do necessary verifications
+	 */
+	public function index()
+	{
+		if ($this->input->post())
+		{
+			$data = $this->uploader();
+		}
+
+		$data['allowed_types'] = str_replace('|', ', ', $this->allowed_types);
+
+		$data['title'] = 'Upload';
+		$data['page'] = 'upload';
+		$this->load->view('view', $data);
+	}
+
+
+	/**
+	 * Executes the uploading process
+	 *
+	 * @return array Returns error or success
+	 */
+	protected function uploader()
+	{
+		$validation = array();
+
+		$config['upload_path'] = APPPATH . 'tmp/upload';
+		$config['allowed_types'] = $this->allowed_types;
+
+		$this->upload->initialize($config);
+
+		if ( ! $this->upload->do_upload('upload'))
+		{
+			$validation['error'] = $this->upload->display_errors();
+		}
+		else
+		{
+			$this->upload->data();
+			$validation['success'] = TRUE;
+		}
+
+		return $validation;
+	}
+
+}
---
+++ private/application/views/pages/upload.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Codestrap
+ *
+ * @package    Codestrap
+ * @author     Natan Felles
+ * @link       https://github.com/natanfelles/codestrap
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<?php if (isset($error)): ?>
+	<div class="col-md-12 alert alert-danger alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		</button>
+		<ul>
+			<?=isset($error) ? "<li>{$error}</li>" : '';?>
+		</ul>
+	</div>
+<?php elseif (isset($success)): ?>
+	<div class="col-md-12 alert alert-success alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		</button>
+		<strong>Yeah!</strong> Your file has ben uploaded.
+	</div>
+<?php else: ?>
+	<div class="col-md-12 alert alert-info alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		</button>
+		<ul>
+			<li>The maximum upload file size allowed by PHP is <strong><?=ini_get("upload_max_filesize");?></strong>.</li>
+			<li>The file extensions allowed are: <strong><?=$allowed_types;?></strong>.</li>
+		</ul>
+	</div>
+<?php endif; ?>
+<form action="" method="post" class="form-horizontal" enctype="multipart/form-data" accept-charset="utf-8">
+	<div class="col-md-12">
+		<div class="panel panel-default">
+			<div class="panel-heading">Select your file</div>
+			<div class="panel-body">
+				<div class="form-group">
+					<label for="upload" class="col-md-2 control-label">File</label>
+					<input type="file" name="upload" id="upload">
+					<input type="hidden" name="hidden" style="display: none">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-12">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<button type="submit" class="btn btn-success">
+					<i class="glyphicon glyphicon-ok-sign"></i> Upload
+				</button>
+			</div>
+		</div>
+	</div>
+</form>
