fc8a95ee0a8e31eac6b4a01fca8615b23acbb1e9
Natan Felles <natanfelles@gmail.com>
2016-03-29 02:37:16 -0300

    Add a simple system error check

--- private/application/controllers/Home.php
+++ private/application/controllers/Home.php
@@ -12,13 +12,31 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$data['message'] = 'Hello, world!';
-		$data['notice'] = 'Everything is working fine.';
+		$data['error'] = $this->check();
 
 		// Config and Load view
 		$data['title'] = 'Home';
 		$data['page'] = 'home';
 		$this->load->view('view', $data);
+	}
+
+
+	protected function check()
+	{
+		$error = NULL;
+
+		// Check file permissions
+		$this->load->helper('file');
+		$tmp_path = APPPATH . 'tmp';
+		$cache_path = ! empty($this->config->config['cache_path']) ? $this->config->config['cache_path'] : APPPATH . 'cache';
+		$log_path = ! empty($this->config->config['log_path']) ? $this->config->config['log_path'] : APPPATH . 'logs';
+		$session_path = ! empty($this->config->config['sess_save_path']) ? $this->config->config['sess_save_path'] : APPPATH . 'session';
+		octal_permissions(fileperms($tmp_path)) == 777 ? : $error[] = 'The file permissions in <strong>' . $tmp_path . '</strong> is not <strong>777</strong>';
+		octal_permissions(fileperms($cache_path)) == 777 ? : $error[] = 'The file permissions in <strong>' . $cache_path . '</strong> is not <strong>777</strong>';
+		octal_permissions(fileperms($log_path)) == 777 ? : $error[] = 'The file permissions in <strong>' . $log_path . '</strong> is not <strong>777</strong>';
+		octal_permissions(fileperms($session_path)) == 777 ? : $error[] = 'The file permissions in <strong>' . $session_path . '</strong> is not <strong>777</strong>';
+
+		return $error;
 	}
 
 }
--- private/application/views/pages/home.php
+++ private/application/views/pages/home.php
@@ -7,12 +7,25 @@
  * @link       https://github.com/natanfelles/codestrap
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
-/**
- * @var string $message
- * @var string $notice
- */
-echo "<h1>{$message}</h1>";
 ?>
-<div class="alert alert-success" role="alert">
-	<p><?=$notice?></p>
-</div>
+<?php if (isset($error)): ?>
+	<h1 class="text-danger"><i class="glyphicon glyphicon-remove"></i> Some errors are breaking your Codestrap</h1>
+	<div class="col-md-12 alert alert-danger alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		</button>
+		<ul>
+			<?php foreach ($error as $item): ?>
+				<li><?=$item?></li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
+<?php else: ?>
+	<h1 class="text-success"><i class="glyphicon glyphicon-ok"></i> Hello, world!</h1>
+	<div class="col-md-12 alert alert-success alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		</button>
+		<strong>Yeah!</strong> Everything is working fine.
+	</div>
+<?php endif; ?>
